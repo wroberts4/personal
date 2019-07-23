acb5168f0d96cc1f3c3779c9e766deb6a14ec03b
wroberts4 <38170479+wroberts4@users.noreply.github.com>
2019-07-22 22:01:13 -0700

    Add firehaus

---
+++ FireHaus/run.bat
@@ -0,0 +1,1 @@
+java -server -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+UseLargePagesInMetaspace -jar server.jar nogui
